1572915dfe9731548ea3601b2b28b2b711dc135e
Yeon <yeoni9094@naver.com>
2024-10-23 00:12:09 +0900

    feat: lv2 패키지에 기존 코드 추가
    요구사항에 맞게 수정할 예정

---
+++ src/lv2/Main.java
@@ -0,0 +1,9 @@
+package lv2;
+
+public class Main {
+    public static void main(String[] args) {
+        // 객체를 생성하고, play() 메서드를 사용해 게임 실행
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.play();
+    }
+}
